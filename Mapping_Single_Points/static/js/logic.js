3276032df013b693182c2da30a8c6871885289a5
Ping Lin <pingyao_lin@yahoo.com>
2020-04-06 23:22:25 -0700

    add circle on LA

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -5,7 +5,25 @@ console.log("working");
 // approximate geographical center of the United States, wz zoom level of “4” 
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+//  Add a marker or circle to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Option1: L.circle 
+//L.circle([34.0522, -118.2437], {
+//	radius: 100
+// }).addTo(map);
+// Option2: L.circleMarker
+L.circleMarker([34.0522, -118.2437],{
+	radius: 300,
+	// black line
+	color: "black",
+	// light yellow circle
+	fillColor: '#ffffa1'
+}).addTo(map);
+
+
 // We create the tile layer (a street level map) that will be the background of our map.
+// for dar map view, use dark-v10 instead of streets-v11
+//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
